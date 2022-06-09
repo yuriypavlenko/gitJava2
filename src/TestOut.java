af15576dd973037a7517cec4349ffba5a45182fc
Yuriy Pavlenko <yurii.pavlenko@mustpay.tech>
2022-06-09 21:39:21 +0300

    add out test4

--- src/TestOut.java
+++ src/TestOut.java
@@ -2,5 +2,6 @@ public class TestOut {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("test2");
+        System.out.println("test4");
     }
 }
